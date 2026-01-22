9f96a6bec5eee13879dafd93911abecbe1e66058
Alegria1001 <euricoalegria98@gmail.com>
2026-01-22 20:53:46 +0100

    Grupo nº3 Pre-processador para includes relativos, remoção de comentarios e define

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import Lexer from "./lexer/Lexer";
 import Parser from "./parser/Parser";
 import SemanticAnalyzer from "./semantic/Semantic";
+import { Preprocessor } from "./Preprocessador/processador";
+
 import fs from "fs";
 import path from "path";
 
-
 const filePath = path.join(__dirname, "input", "code.nt");
-const code = fs.readFileSync(filePath, "utf-8");
+const rawCode = fs.readFileSync(filePath, "utf-8");
 
-const lexer = new Lexer(code);
+const preprocessor = new Preprocessor([path.join(__dirname, "input")]);
+const cleanCode = preprocessor.processFile(filePath);
+
+fs.writeFileSync(filePath + ".pp", cleanCode);
+
+const lexer = new Lexer(cleanCode);
 const parser = new Parser(lexer);
 const ast = parser.parse();
 
---
+++ src/input/code.nt.pp
@@ -0,0 +1,5 @@
+let z = 10 + 5;
+let x = 3;
+let y = x / 1;
+print 10 + 5;
+print y;
